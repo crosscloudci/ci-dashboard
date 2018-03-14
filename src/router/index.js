760f58f28c25465daf3e5ee4379deea963b57076
JoshuaDarius <sup@joshuadarius.com>
2018-03-14 10:23:36 -0400

    added deploy demo screen along with vue route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MainTable from '@/components/MainTable'
 import DemoTable from '@/components/DemoTable'
+import DeployTable from '@/components/deployDemo/DeployTable'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'MainTable',
       component: MainTable
+    },
+    {
+      path: '/deploy*',
+      name: 'demo-deploy',
+      component: DeployTable
     },
     {
       path: '/running*',
