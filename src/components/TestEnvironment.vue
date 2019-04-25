c13dd3d9ea0b8e31781fbe947f10b38944e3bc4e
Lucina <lixuna@users.noreply.github.com>
2019-04-25 15:56:30 -0500

    update K8s logo w/ project directory URL
    crosscloudci/ci-dashboard#90
    
    Changes made: add /projects/ to URL
    
    Line 7:            ``` <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/projects/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'" />```
    
    
    Line 27: ```            <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/projects/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'"```

--- src/components/TestEnvironment.vue
+++ src/components/TestEnvironment.vue
@@ -4,7 +4,7 @@
           <span class="test-env-label">Test environment</span>
           <div class="test-env-name" @click="openDialog('dialog1')">
             <div class="icon">
-            <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'" />
+            <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/projects/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'" />
             </div>
             <div>
               Kubernetes &mdash; 
@@ -24,7 +24,7 @@
           <span class="test-env-label">Test environment</span>
           <div class="test-env-name" v-on:click="gotoProjectURL()">
             <div class="icon">
-            <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'"
+            <img :src="'https://raw.githubusercontent.com/cncf/artwork/master/projects/kubernetes/icon/color/kubernetes-icon-color.svg?sanitize=true'"
             />
             </div>
             <div>
