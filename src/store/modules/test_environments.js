b01b2b00ef9b8bcd0670898d2e16e9e779beeac7
Joshua Darius <sup@joshuadarius.com>
2019-05-10 18:18:50 -0400

    updated vuex current env reducer to check for selected env before update in case of project payload mutation; *- fixed conditional typo

--- src/store/modules/test_environments.js
+++ src/store/modules/test_environments.js
@@ -61,7 +61,7 @@ const mutations = {
     for (let i = 0; i < list.length; i++) {
       let hi = R.match(/Stable/, list[i].dropdown)
       if (hi.length > 0) {
-        if (state.current.length !== 0) {
+        if (state.current.length === 0) {
           state.current = list[i]
           return state.current
         }
